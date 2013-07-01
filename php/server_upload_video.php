87eb34d74e01544582af9d38558432e50b2b1d66
savorks <savorksp@hotmail.com>
2013-07-01 15:31:53 +0800

    added comments in server_upload_video.php

--- php/server_upload_video.php
+++ php/server_upload_video.php
@@ -1,31 +1,38 @@
 <?php
 
 error_reporting(E_ERROR);  
-//error_reporting(E_ALL);  
 
 ini_set('memory_limit', '256M');
 ini_set("max_execution_time", "60");
 
-//$uploadPath = tempnam("temp/", "FOO");
-
 $success = 0;
 $result = '';
 
+// recived binary data from swf
 $im = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-//if (move_uploaded_file($_FILES['Filedata']['tmp_name'], $uploadPath)) 
 if ($im) 
 {
+	// temporary save file inside temp folder
 	$filename = "../temp/tempfile".time()."x.flv";
 	$fp = fopen($filename, 'w'); 
 	fwrite($fp, $im);
 	fclose($fp);
 	
+	/*	include serects and youtube php api
+		the serects.php file contains these informations:
+		$_email			your google account name(email)
+		$_pwd			your google account password
+		$_developerKey	the developer key we use for uploading
+	*/
 	require_once("private/serects.php");
 	require_once('Zend/Loader.php');
 	Zend_Loader::loadClass('Zend_Gdata_YouTube');
 	Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
 
+	/*	use your google account request a client login into youtube, get authorization/accessToken then create httpclient, 
+		which we need for initializing YouTube instance.
+	*/
 	$authenticationURL= 'https://www.google.com/accounts/ClientLogin';
 	$httpclient = Zend_Gdata_ClientLogin::getHttpClient(
 					  $username = $_email,
@@ -38,22 +45,20 @@ if ($im)
 					  $authenticationURL
 				   );
 	
-	
 	$developerKey = $_developerKey;
 	$applicationId = 'Upload Flv Test';
 	$clientId = 'My video upload client - v1';
 	
 	
-	
+	// create YouTube instance, create video entry, assign datas for our new video entry
 	$yt = new Zend_Gdata_YouTube($httpclient, "Upload Flv Test", NULL, $developerKey);
 	
 	$myVideoEntry = new Zend_Gdata_YouTube_VideoEntry();
 	
+	$myVideoEntry->setMediaSource($filesource);
 	$filesource = $yt->newMediaFileSource($filename);
 	$filesource->setContentType('video/x-flv');
 	$filesource->setSlug('resultx.flv');
-	
-	$myVideoEntry->setMediaSource($filesource);
 	
 	$myVideoEntry->setVideoTitle('My Test Movie');
 	$myVideoEntry->setVideoDescription('My Test Movie');
@@ -61,17 +66,9 @@ if ($im)
 	$myVideoEntry->SetVideoTags('cars, funny');
 	$myVideoEntry->setVideoDeveloperTags(array('mydevtag', 'anotherdevtag'));
 	
-	/*
-	$yt->registerPackage('Zend_Gdata_Geo');
-	$yt->registerPackage('Zend_Gdata_Geo_Extension');
-	$where = $yt->newGeoRssWhere();
-	$position = $yt->newGmlPos('37.0 -122.0');
-	$where->point = $yt->newGmlPoint($position);
-	$myVideoEntry->setWhere($where);
-	*/
-	
 	$uploadUrl = 'http://uploads.gdata.youtube.com/feeds/api/users/default/uploads';
 	
+	// start upload new entry into youtube, if success we will get new video state, which contains information like video id, we can use it generate url to the video site
 	try 
 	{
 		$newEntry = $yt->insertEntry($myVideoEntry, $uploadUrl, 'Zend_Gdata_YouTube_VideoEntry');
@@ -104,7 +101,5 @@ function endWith($success, $result)
 	if($filename) unlink($filename);
 	die("s=".$success."&res=".$result);
 }
-
-
 
 ?>
