770420a3811e4e15447e30599e48f0f17c3bbd1a
sav-scripts <savorksp@hotmail.com>
2013-07-01 22:05:49 +0800

    Update serects.php

--- php/private/serects.php
+++ php/private/serects.php
@@ -1,7 +1,7 @@
 <?php
 
-$_email = "savtesterx@gmail.com";
-$_pwd = "tester99";
-$_developerKey = "AI39si4Jwo_Qk6gHYlUeU4jlG7tcWFzeeE2RT8xVzFgX_NGVw-xHnZZxfOAhBncpWWAo06QAJhRwIYSUsZQ3kM39hK2g6hpcGw";
+$_email = "your google account";
+$_pwd = "account password";
+$_developerKey = "developer key";
 
 ?>
